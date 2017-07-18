0db9da6860bbcedde3a005112bd8b76a1de06f60
dubrzr <contact@juliendubiel.net>
2017-07-18 18:52:47 +0200

    Update working script.

--- create.sh
+++ create.sh
@@ -1,14 +1,17 @@
-# Variables
+#!/bin/bash
 
-LINUX_USER = leech;
-MAIN_DIR = /home/webapps/apps/leech;
-OUTPUT_DIR = $MAIN_DIR/output;
-DOWNLOADS_DIR = $OUTPUT_DIR/downloads;
-TZ = Europe/Paris;
-LISTEN = 127.0.0.1;
-D_SUBNET = 172.50.0;
-D_DNS = 8.8.8.8;
-D_NETWORK_NAME = leech;
+
+# VARIABLES
+linux_user="leech";
+main_dir="/home/webapps/apps/leech";
+output_dir="$main_dir/output";
+downloads_dir="$output_dir/downloads";
+tz="europe/paris";
+listen="127.0.0.1";
+d_subnet="172.50.0";
+d_dns="8.8.8.8";
+d_network_name="leech";
+
 
 # CHECK RIGHTS
 if [[ $EUID -ne 0 ]]; then
@@ -16,45 +19,47 @@ if [[ $EUID -ne 0 ]]; then
    exit 1;
 fi
 
-# CREATE USER IF NEEDED
-if ! grep $LINUX_USER /etc/passwd; then
-  echo "User $LINUX_USER does not exists, please create it!" 1>&2;
+
+# GET USER UID AND GID
+if ! grep $linux_user /etc/passwd &>/dev/null; then
+  echo "User $linux_user does not exists, please create it!" 1>&2;
   exit 2;
 fi
-UID = id -u $LINUX_USER;
-GID = id -g $LINUX_USER;
+uid=$(id -u $linux_user);
+gid=$(id -g $linux_user);
 
 
 # CREATE LOCAL FOLDERS
-mkdir -p $OUTPUT_DIR/tv;
-mkdir -p $OUTPUT_DIR/movies;
-mkdir -p $DOWNLOADS_DIR/completed;
+mkdir -p $output_dir/tv;
+mkdir -p $output_dir/movies;
+mkdir -p $downloads_dir/completed;
+
 
 # CREATE DOCKER SUBNET
-sudo docker network create --subnet $D_SUBNET.0/16 $D_NETWORK_NAME;
+sudo docker network create --subnet $d_subnet.0/16 $d_network_name; 
 
 # DELUGE
-mkdir -p $MAIN_DIR/deluge/appdata;
-sudo docker create --name deluge -v $MAIN_DIR/deluge/appdata:/config -v $DOWNLOADS_DIR:/downloads -e PGID=$GID -e PUID=$UID -e TZ=$TZ -p $LISTEN:6906:8112 --ip $D_SUBNET.56 --network $D_NETWORK_NAME --dns $D_DNS linuxserver/deluge
+mkdir -p $main_dir/deluge/appdata;
+sudo docker create --name deluge -v $main_dir/deluge/appdata:/config -v $downloads_dir:/downloads -e pgid=$gid -e puid=$uid -e tz=$tz -p $listen:6906:8112 --ip $d_subnet.56 --network $d_network_name --dns $d_dns linuxserver/deluge
 
 # NZBGET
-mkdir -p $MAIN_DIR/nzbget/appdata;
-sudo docker create --name nzbget -v $MAIN_DIR/nzbget/appdata:/config -v $DOWNLOADS_DIR:/downloads -e PGID=$GID -e PUID=$UID -e TZ=$TZ -p $LISTEN:6901:6789 --ip $D_SUBNET.51 --network $D_NETWORK_NAME --dns $D_DNS linuxserver/nzbget
+mkdir -p $main_dir/nzbget/appdata;
+sudo docker create --name nzbget -v $main_dir/nzbget/appdata:/config -v $downloads_dir:/downloads -e pgid=$gid -e puid=$uid -e tz=$tz -p $listen:6901:6789 --ip $d_subnet.51 --network $d_network_name --dns $d_dns linuxserver/nzbget
 
 # HYDRA
-mkdir -p $MAIN_DIR/nzbhydra/appdata;
-sudo docker create --name hydra -v $MAIN_DIR/nzbhydra/appdata/:/config -v $DOWNLOADS_DIR/completed:/downloads -e PGID=$GID -e PUID=$UID -e TZ=$TZ -p $LISTEN:6905:5075 --ip $D_SUBNET.55 --network $D_NETWORK_NAME --dns $D_DNS linuxserver/hydra
+mkdir -p $main_dir/nzbhydra/appdata;
+sudo docker create --name hydra -v $main_dir/nzbhydra/appdata/:/config -v $downloads_dir/completed:/downloads -e pgid=$gid -e puid=$uid -e tz=$tz -p $listen:6905:5075 --ip $d_subnet.55 --network $d_network_name --dns $d_dns linuxserver/hydra
 
 # SONARR
-mkdir $MAIN_DIR/sonarr/appdata;
-sudo docker create --name sonarr -v $MAIN_DIR/sonarr/appdata:/config -v $DOWNLOADS_DIR/completed:/downloads -v $OUTPUT_DIR/tv:/tv -e PGID=$GID -e PUID=$UID -e TZ=$TZ -v /etc/localtime:/etc/localtime:ro -p $LISTEN:6902:6902 --ip $D_SUBNET.52 --network $D_NETWORK_NAME --dns $D_DNS linuxserver/sonarr
+mkdir -p $main_dir/sonarr/appdata;
+sudo docker create --name sonarr -v $main_dir/sonarr/appdata:/config -v $downloads_dir/completed:/downloads -v $output_dir/tv:/tv -e pgid=$gid -e puid=$uid -e tz=$tz -v /etc/localtime:/etc/localtime:ro -p $listen:6902:6902 --ip $d_subnet.52 --network $d_network_name --dns $d_dns linuxserver/sonarr
 
 # RADARR
-mkdir $MAIN_DIR/radarr/appdata;
-sudo docker create --name radarr -v $MAIN_DIR/radarr/appdata:/config -v $DOWNLOADS_DIR/completed:/downloads -v $OUTPUT_DIR/movies:/movies -e PGID=$GID -e PUID=$UID TZ=$TZ -v /etc/localtime:/etc/localtime:ro -p $LISTEN:6907:7878 --ip $D_SUBNET.57 --network $D_NETWORK_NAME --dns $D_DNS linuxserver/radarr;
+mkdir -p $main_dir/radarr/appdata;
+sudo docker create --name radarr -v $main_dir/radarr/appdata:/config -v $downloads_dir/completed:/downloads -v $output_dir/movies:/movies -e pgid=$gid -e puid=$uid -e tz=$tz -v /etc/localtime:/etc/localtime:ro -p $listen:6907:7878 --ip $d_subnet.57 --network $d_network_name --dns $d_dns linuxserver/radarr;
 
-sudo chown -R $MAIN_DIR;
-sudo chmod -R 750 $MAIN_DIR; 
+sudo chown -R $linux_user $main_dir;
+sudo chmod -R 750 $main_dir; 
 
 sudo systemctl enable leech@deluge;
 sudo systemctl start leech@deluge;
